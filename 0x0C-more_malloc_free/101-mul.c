a89c608769c34978d10736b03777d653a90e40d6
fidelismarkcen <markcenfidelis@gmail.com>
2023-01-13 02:59:19 -0800

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,114 +1,97 @@
-
-Superstazdare
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x0C-more_malloc_free/101-mul.c
-@Superstazdare
-Superstazdare task 5
- 1 contributor
-97 lines (93 sloc)  1.65 KB
-#include "main.h"
-#include <stdlib.h>
+#include "holberton.h"
 #include <stdio.h>
-#define ERR_MSG "Error"
-
+#include <stdlib.h>
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * argstostr - concatenates all the arguments
+ * @ac: argument count
+ * @av: arguments passed
+ * Return: pointer to a string or NULL
  */
-int is_digit(char *s)
+char *argstostr(int ac, char **av)
 {
-	int i = 0;
+	char *mem;
+	unsigned int total;
 
-	while (s[i])
+	if (ac == 0 || av == NULL)
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
+		return (NULL);
 	}
-	return (1);
+	total = total_length(ac, av);
+	mem = malloc(total * sizeof(char));
+	copy_string(av, mem);
+	return (mem);
 }
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
+ * _strlen - getting str length of every argv
+ * @s: the string
+ * Return: str len
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
 /**
- * errors - handles errors for main
+ * total_length - total length of all argv
+ * @ac: counts argv
+ * @av: arguments
+ * Return: sum
  */
-void errors(void)
+unsigned int total_length(int ac, char **av)
 {
-	printf("Error\n");
-	exit(98);
+	int i;
+	unsigned int sum;
+
+	i = 0;
+	sum = 0;
+	while (av[i] != NULL)
+	{
+		sum = sum + _strlen(av[i]);
+		i++;
+	}
+	sum = sum + ac + 1;
+	return (sum);
 }
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ * copy_string - concat strings
+ * @av: arguments
+ * @s: string
+ * Return: str values
  */
-int main(int argc, char *argv[])
+void copy_string(char **av, char *s)
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int i;
+	int k;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	i = 0;
+	k = 0;
+	while (av[i] != NULL)
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+		from_to(av[i], s + k);
+		k = k + _strlen(av[i]);
+		s[k] = '\n';
+		k++;
+		i++;
 	}
-	for (i = 0; i < len - 1; i++)
+	s[k] = '\0';
+}
+/**
+ * from_to - copies values
+ * @origin: copy from
+ * @dest: copy to
+ * Return: nothing
+ */
+void from_to(char *origin, char *dest)
+{
+	int i;
+
+	i = 0;
+	while (origin[i] != '\0')
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
+		dest[i] = origin[i];
+		i++;
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
-	return (0);
 }
