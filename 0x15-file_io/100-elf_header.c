f1557bc086e7440a12cb9f25ae1fe2042c69d27c
fidelismarkcen <markcenfidelis@gmail.com>
2023-02-07 19:12:41 -0800

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,20 +1,3 @@
-
-Superstazdare
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x15-file_io/100-elf_header.c
-@Superstazdare
-Superstazdare files
- 1 contributor
-314 lines (286 sloc)  6.69 KB
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
