018c369fc00f4ce67f418fa9bc83331d4d4cbca0
root <fidelismaekcen@github.com>
2024-09-25 17:59:05 -0700

    Implement print function for doubly linked list

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -27,8 +27,10 @@ int main(void)
     new->next = head;
     new->prev = NULL;
     head = new;
+
     n = print_dlistint(head);
     printf("-> %lu elements\n", n);
+
     free(new);
     return (EXIT_SUCCESS);
 }
--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,22 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - function that prints all the elemts of a doubly linked list
- * @h: poiner to the head of the list
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the doubly linked list.
  *
- * Return: number of nodes
+ * Return: The number of nodes in the list.
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
-	/* while we have a pointer to the list */
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
-	}
-	return (count);
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);  /* Print the current node's value */
+        count++;               /* Increment the node count */
+        h = h->next;           /* Move to the next node */
+    }
+    
+    return count;              /* Return the total count of nodes */
 }
